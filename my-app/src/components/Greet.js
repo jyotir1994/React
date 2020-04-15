9433394c68418d9fd15dce2a626a73c887645f91
impressico <jyotir1994@gmail.com>
2020-04-16 01:05:40 +0530

    components

---
+++ my-app/src/components/Greet.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+// Normal Function
+// function Greet(){
+    // return <h1>Hello Jyoti</h1>
+// }
+
+//Arrow function
+const Greet = () => <h1>hello</h1>
+
+export default Greet;
