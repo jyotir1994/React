62527e50e0a6ba74425d1f9d1cd82cab982768f1
impressico <jyotir1994@gmail.com>
2020-04-17 19:39:17 +0530

    styling and css basics

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -12,11 +12,19 @@ import EventBind from './components/EventBind';
 import ParentComponent from './components/ParentComponent';
 import UserGreetings from './components/UserGreetings';
 import NameList from './components/NameList';
+import Stylesheet from './components/Stylesheet';
+import Inline from './components/Inline';
+import './appStyles.css';
+import styles from './appStyles.module.css';
 
 function App() {
   return (
     <div className="App">
-      <NameList />
+      <h1 className={'error'}>Error</h1>
+      <h1 className={styles.success}>Success</h1>
+      <Inline />
+      <Stylesheet primary={true}/>
+      {/* <NameList /> */}
       {/* <UserGreetings /> */}
       {/* <ParentComponent /> */}
       {/* <EventBind /> */}
