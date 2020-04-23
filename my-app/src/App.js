f8599ed6ded1b16ba5c622fe299eb7c0dee12d5e
impressico <jyotir1994@gmail.com>
2020-04-23 16:41:58 +0530

    react portal

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
 import FRParentInput from './components/FRParentInput';
+import PortalDemo from './components/PortalDemo';
 
 function App() {
   return (
     <div className="App">
-      <FRParentInput />
+      <PortalDemo />
     </div>
   );
 }
