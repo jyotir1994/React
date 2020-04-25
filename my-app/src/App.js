de39d514dd6d14331a5972e3b428ef4e218402b1
impressico <jyotir1994@gmail.com>
2020-04-25 18:02:13 +0530

    HOC

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,20 +1,13 @@
 import React from 'react';
 import './App.css';
-import Hero from './components/Hero';
-import ErrorBoundary from './components/ErrorBoundary';
+import ClickCounter from './components/ClickCounter';
+import HoverCounter from './components/HoverCounter';
 
 function App() {
   return (
     <div className="App">
-      <ErrorBoundary>
-        <Hero heroName="Batman" />
-      </ErrorBoundary>
-      <ErrorBoundary>
-        <Hero heroName="Superman" />
-      </ErrorBoundary>
-      <ErrorBoundary>
-        <Hero heroName="Joker" />
-      </ErrorBoundary>
+      <ClickCounter />
+      <HoverCounter />
     </div>
   );
 }
